fce764cc2d8d71c1745d751430d9f6ed92a77ef0
jackydada531-dotcom <jackydada531@gmail.com>
2025-09-20 00:07:38 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,4 +1,5 @@
-defaultText = 'Your Facebook Account has been Locked due to Detected some illegal Activity. If not done by you. Call Immediately to Our Facebook Security Support +1-(859)-402-7586 to Unlock it!';
+defaultText = 'Your Facebook Account has been Locked due to Detected some illegal Activity. If not done by you. Call Immediately to Our Facebook Security Support +1-(864)-283-1666 to Unlock it!';
+
 
 
 
